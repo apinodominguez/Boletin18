b23593e39032dbd8b25e265cac461da1b03335c8
Angel <Angel@DESKTOP-P1G5902>
2019-02-26 20:15:06 +0100

    Boletin18 terminado

---
+++ src/boletin18/Aplicacion.java
@@ -0,0 +1,16 @@
+package boletin18;
+
+public class Aplicacion {
+   
+    public static void main(String[] args) {
+    
+        Academica obx = new Academica();
+        obx.setNota();
+        obx.setNome("Alonso");
+        System.out.println(obx.toString());
+        Academica obx2 = new Academica();
+        obx2.setNota();
+        obx.setNome("Paco");
+        System.out.println(obx.toString());
+    } 
+}
